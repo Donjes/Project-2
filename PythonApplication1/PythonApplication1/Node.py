6eb8aa61d1b4556dc41b6b789b34335fbee4a35e
Richard <richardyuan_@hotmail.com>
2016-01-22 12:26:05 +0100

    comments

--- PythonApplication1/PythonApplication1/Node.py
+++ PythonApplication1/PythonApplication1/Node.py
@@ -32,7 +32,7 @@ class Tile_interaction:
     def __init__(self,tnr,action):
         self.tileNum = tnr
         self.Action = action
-# VALUES FOR THE "fightnumber" in class FightingTile
+#  ALL POSSIBLE VALUES FOR THE "fightnumber" in class FightingTile
 0 = CornerFight 
 1 = SpotFight
 2 = SuperFight
