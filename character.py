f22417b6f5eb810f6ad5df5e4057d4e7f4c12506
Richard <richardyuan_@hotmail.com>
2016-01-25 10:46:58 +0100

    New file for character and charactercard classes.

---
+++ character.py
@@ -0,0 +1,13 @@
+class Tile_interaction:
+    def __init__(self,tnr,action):
+        self.tileNum = tnr
+        self.Action = action
+#  ALL POSSIBLE VALUES FOR THE "fightnumber" in class FightingTile
+# 0 = CornerFight 
+# 1 = SpotFight
+# 2 = SuperFight
+
+class FightingTile:
+    def __init__(self,tile,fightnumber,tnr,action):
+        self.tile = Tile_interaction(tnr,action)
+        self.fightingNumber = fightnumber
