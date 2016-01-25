8a956c061212159527bd5249e1c5771b884e07d9
Richard <richardyuan_@hotmail.com>
2016-01-25 10:48:34 +0100

    Update Node.py
    Only class Node and class Empty

--- PythonApplication1/PythonApplication1/Node.py
+++ PythonApplication1/PythonApplication1/Node.py
@@ -4,42 +4,8 @@ class Node:
     self.Value = value
     self.IsEmpty = False
 
-
-
 class Empty: 
   def __init__(self):
     self.IsEmpty = True
 
 Empty = Empty()
-
-class Character:
-    def __init__(self,card,eye1,eye2,eye3,eye4,eye5,eye6):
-        self.card = characterCard(eye1,eye2,eye3,eye4,eye5,eye6)
-        self.hitPoints = 100
-        self.conditionPoints = 15 
-        self.SavePosition = None #Optional for a saving option
-
-class characterCard:
-    def __init__(self,attack1,attack2,attack3,attack4,attack5,attack6):
-        self.Dice1 = attack1
-        self.Dice2 = attack2
-        self.Dice3 = attack3
-        self.Dice4 = attack4
-        self.Dice5 = attack5
-        self.Dice6 = attack6
-
-class Tile_interaction:
-    def __init__(self,tnr,action):
-        self.tileNum = tnr
-        self.Action = action
-#  ALL POSSIBLE VALUES FOR THE "fightnumber" in class FightingTile
-0 = CornerFight 
-1 = SpotFight
-2 = SuperFight
-
-
-
-class FightingTile:
-    def __init__(self,tile,fightnumber,tnr,action):
-        self.tile = Tile_interaction(tnr,action)
-        self.fightingNumber = fightnumber
