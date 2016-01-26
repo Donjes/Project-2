f3aec599526df17895a860abef367d78e2ab2f79
Edgar <edgarbuyten@gmail.com>
2016-01-26 10:33:17 +0100

    Wachten op texture.png
    Texture attribute added, waiting for .png files to implement in this code

--- PythonApplication1/PythonApplication1/character.py
+++ PythonApplication1/PythonApplication1/character.py
@@ -1,9 +1,10 @@
 class Character:
-    def __init__(self,eye1,eye2,eye3,eye4,eye5,eye6):
+    def __init__(self,eye1,eye2,eye3,eye4,eye5,eye6, texture):
         self.card = characterCard(eye1,eye2,eye3,eye4,eye5,eye6)
         self.hitPoints = 100
         self.conditionPoints = 15
         self.SavePosition = None #Optional for a saving option
+        self.texture = texture
 
 class characterCard:
     def __init__(self,attack1,attack2,attack3,attack4,attack5,attack6):
@@ -14,11 +15,11 @@ class characterCard:
         self.Dice5 = attack5
         self.Dice6 = attack6
 
-TerryCrews = Character(10, 15, 25, 30, 20, 10)
-JasonStatham = Character(10, 11, 19, 21, 23, 26)
-WesleySniper = Character(30, 14, 14, 20, 18, 14)
-JetRi = Character(10, 30, 12, 25, 10, 23)
-StevenSeagal = Character(27, 15, 12, 11, 25, 20)
-SuperMario = Character(10, 10, 30, 30, 15, 15)
-VinDieser = Character(20, 25, 5, 25, 20, 15)
-ChackNorris = Character(10, 26, 25, 24, 24, 1)
+TerryCrews = Character(10, 15, 25, 30, 20, 10, TerryCrews.png)
+JasonStatham = Character(10, 11, 19, 21, 23, 26, JasonStatham.png)
+WesleySniper = Character(30, 14, 14, 20, 18, 14, WesleySniper.png)
+JetRi = Character(10, 30, 12, 25, 10, 23, JetRi.png)
+StevenSeagal = Character(27, 15, 12, 11, 25, 20, StevenSeagal.png)
+SuperMario = Character(10, 10, 30, 30, 15, 15, SuperMario.png)
+VinDieser = Character(20, 25, 5, 25, 20, 15, VinDieser.png)
+ChackNorris = Character(10, 26, 25, 24, 24, 1, ChackNorris.png)
