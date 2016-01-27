b2578834fa2ed75df71a3e4bc10397ee2d9ada60
rubenev <mac_fley@hotmail.com>
2016-01-27 11:55:33 +0100

    navigatie en characters update

--- PythonApplication1/PythonApplication1/boardnavigatie.py
+++ PythonApplication1/PythonApplication1/boardnavigatie.py
@@ -2,10 +2,13 @@ import pygame
 import time
 from Dice import *
 import os
-#from Node import *
+from character import *
+from Node import *
 
 pygame.init()
-
+playerList = Empty
+player1 = TerryCrews
+player2 = JasonStatham
 size = width, height = 700, 650
 tile = 0
 white = (255, 255, 255)
@@ -16,31 +19,50 @@ clock = pygame.time.Clock()
 roll = Trow_dice()
 board_screen = pygame.image.load("images/speelveld.png")
 board_screen_rect = board_screen.get_rect()
+def small_glove(gloveSmall,navi):                  #3Ruben handschoen over board functie    
+        gameDisplay.blit(pygame.image.load(gloveSmall),navi)
+def dice_img(roll):
+        gameDisplay.blit(pygame.image.load("images/" + roll[1]),(300,300))
+
+playerList = Node(player1,Node(player2,Empty))
 
 while True:
-    def small_glove(navi):                  #3Ruben handschoen over board functie    
-         return gameDisplay.blit(gloveSmall,navi)
-    def dice_img(roll):
-         gameDisplay.blit(pygame.image.load("images/" + roll[1]),(300,300))
-
-    navigate = [(20,20),(80,20),(130,20),(180,20),(230,20),(300,20),(375,20),(425,20),(472,20),(520,20),(575,20),(575,85),(575,130),(575,180),(575,230),(575,300),(575,373),(575,425),(575,475),(575,525),(575,590)]
-
-    for event in pygame.event.get():         
-        if event.type == pygame.KEYDOWN:  
-            roll = Trow_dice()  
-            if event.key == pygame.K_SPACE: 
-                for i in range(roll[0]):
-                    tile += 1 
-                    navi = navigate[tile%21]
-                    small_glove(navi)    
-                    pygame.display.update()
-                    time.sleep(0.1)
-                print("plus")                          
-
+        player = playerList.Value
+        navigate = [(20,20),(80,20),(130,20),(180,20),(230,20),(300,20),(375,20),(425,20),(472,20),(520,20),(575,20),(575,85),(575,130),(575,180),(575,230),(575,300),(575,373),(575,425),(575,475),(575,525),(575,590)]
+        if player == player1:
+            gloveSmall = "images/red_handschoen.png"
+            for event in pygame.event.get():         
+                if event.type == pygame.KEYDOWN:         
+                    if event.key == pygame.K_SPACE: 
+                        roll = Trow_dice() 
+                        for i in range(roll[0]):
+                            tile += 1 
+                            navi = navigate[tile%21]
+                            small_glove(gloveSmall,navi)    
+                            pygame.display.update()
+                            time.sleep(0.1)
+                            print("plus")  
+                        player.Position = navi
+                        playerList = playerList.Tail
+        if player == player2:
+            gloveSmall = "images/blue_handschoen.png"
+            for event in pygame.event.get():         
+                if event.type == pygame.KEYDOWN:         
+                    if event.key == pygame.K_SPACE: 
+                        roll = Trow_dice() 
+                        for i in range(roll[0]):
+                            tile += 1 
+                            navi = navigate[tile%21]
+                            small_glove(gloveSmall,navi)    
+                            pygame.display.update()
+                            time.sleep(0.1)
+                            print("plus")    
+                        player.Position = navi                                
+                        playerList = playerList.Tail
         print(navi)
 
         gameDisplay.blit(pygame.image.load("images/speelveld.png"),board_screen.get_rect()) 
-        small_glove(navi)
+        small_glove(gloveSmall,navi)
         dice_img(roll)
         pygame.display.update()
         clock.tick(60)
--- PythonApplication1/PythonApplication1/character.py
+++ PythonApplication1/PythonApplication1/character.py
@@ -1,6 +1,7 @@
 class Player:
-    def __init__(self,name,texture,attack1,attack2,attack3,attack4,attack5,attack6):
+    def __init__(self,name,position,texture,attack1,attack2,attack3,attack4,attack5,attack6):
         self.playername = name
+        self.Position = position
         self.character = Character(texture,attack1,attack2,attack3,attack4,attack5,attack6)
 
 
@@ -17,14 +18,14 @@ class Character:
         self.dice5 = attack5
         self.dice6 = attack6
 
-TerryCrews = Player("input","images/TerryCrews.png",10,15,25,30,20,10)
-JasonStatham = Player("input","images/JasonStatham.png",10,11,19,21,23,26)
-WesleySniper = Player("input","images/WesleySniper.png",30,14,14,20,18,14)
-JetRi = Player("input","images/JetRi.png",10,30,12,25,10,23)
-StevenSeagal = Player("input","images/StevenSeagal.png",27,15,12,11,25,20)
-SuperMario = Player("input","images/SuperMario.png",10,10,30,30,15,15)
-JackieChan = Player("input","images/JackieChan.png",20,25,5,25,20,15)
-ChackNorris = Player("input","images/ChackNorris.png",10,26,25,24,24,1)
+TerryCrews = Player("input",(-100,-100),"images/TerryCrews.png",10,15,25,30,20,10)
+JasonStatham = Player("input",(-100,-100),"images/JasonStatham.png",10,11,19,21,23,26)
+WesleySniper = Player("input",(-100,-100),"images/WesleySniper.png",30,14,14,20,18,14)
+JetRi = Player("input",(-100,-100),"images/JetRi.png",10,30,12,25,10,23)
+StevenSeagal = Player("input",(-100,-100),"images/StevenSeagal.png",27,15,12,11,25,20)
+SuperMario = Player("input",(-100,-100),"images/SuperMario.png",10,10,30,30,15,15)
+JackieChan = Player("input",(-100,-100),"images/JackieChan.png",20,25,5,25,20,15)
+ChackNorris = Player("input",(-100,-100),"images/ChackNorris.png",10,26,25,24,24,1)
 # "input" LINKEN AAN DE "input" VAN DE PLAYERNAME.
 
 
