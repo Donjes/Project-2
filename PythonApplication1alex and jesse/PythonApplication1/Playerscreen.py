5ff2529e9423135dcfbd3d40a598cd0553bf8771
Edgar Buyten <edgarbuyten@gmail.com>
2016-02-01 11:16:28 +0100

    Revert "ping voor intro ingevoegd"
    This reverts commit b94ab7ddb0ebf9f76b2e576f85fd8c837279cca5.

--- PythonApplication1alex and jesse/PythonApplication1/Playerscreen.py
+++ PythonApplication1alex and jesse/PythonApplication1/Playerscreen.py
@@ -7,7 +7,6 @@ punchsound = pygame.mixer.Sound("sounds/punch.ogg")
 dicesound = pygame.mixer.Sound("sounds/dice.ogg")
 mariosound = pygame.mixer.Sound("sounds/mario.ogg")
 tilesound = pygame.mixer.Sound("sounds/tileshift.ogg")
-introping = pygame.mixer.Sound("sounds/introping.ogg")
 
 class Sounds:
     def Punch():
@@ -18,8 +17,6 @@ class Sounds:
         pygame.mixer.Sound.play(mariosound)
     def Tile():
         pygame.mixer.Sound.play(tilesound)
-    def Introping():
-        pygame.mixer.Sound.Play(introping)
 
 
 
