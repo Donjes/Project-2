6d2331c7fd5eb81acc502da9d2fa2a550007fe6c
rubenev <mac_fley@hotmail.com>
2016-02-01 12:22:58 +0100

    Bugs fixed

--- PythonApplication1alex and jesse/PythonApplication1/Game.py
+++ PythonApplication1alex and jesse/PythonApplication1/Game.py
@@ -18,7 +18,7 @@ load_old_game = False
 last_page = 0
 letsSuperFight = 0
 letsFight = 0
-
+nextturn = 0
 #playerList = [player1,player2,player3,player4]
 gameDisplay = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Survivor')
@@ -203,10 +203,11 @@ while not crashed:
 
     elif screen_index == 3:
         #hier moet Gameboard() komen
-        firstround,chooseChars,roll,p,screenlist, rectlist, crashed, menu_index, screen_index,last_page,letsSuperFight,letsFight = \
-        BoardScreen(firstround,chooseChars,roll, p,screenlist, rectlist, crashed, menu_index, screen_index,last_page,letsSuperFight,letsFight)
+        firstround,chooseChars,roll,p,screenlist, rectlist, crashed, menu_index, screen_index,last_page,letsSuperFight,letsFight,nextturn = \
+        BoardScreen(firstround,chooseChars,roll, p,screenlist, rectlist, crashed, menu_index, screen_index,last_page,letsSuperFight,letsFight,nextturn)
         size = width, height = 650, 746
         gameDisplay = pygame.display.set_mode(size)
+        
         gameDisplay.blit(pygame.image.load("images/speelveld.png"),(pygame.image.load("images/speelveld.png").get_rect()))    
         small_glove(gloveSmall1,navigate[chooseChars[0].savePosition%40])
         small_glove(gloveSmall2,navigate[chooseChars[1].savePosition%40])
@@ -218,11 +219,17 @@ while not crashed:
         small_glove(chooseChars[2].texture,(470,470))
         small_glove(chooseChars[3].texture,(110,470))
         dice_img(roll)
+        if nextturn == 1:
+            gameDisplay.blit(pygame.image.load("images/nextturn.png"),(88,225))
+            small_glove(chooseChars[p%4].texture,(150,250))
+            pygame.display.update()
+            time.sleep(1)
+            nextturn = 0
         if letsSuperFight == 1:
             superFight(chooseChars[p%4],chooseChars)
         if letsFight == 1:
             fight(chooseChars[p%4],chooseChars)
-
+        
     elif screen_index == 5:
         if save_game == True:
             saveGame(firstround,chooseChars,roll,p,screenlist, rectlist, crashed, menu_index, screen_index)
